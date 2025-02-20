27abb7bc7770d23ea4931bce9cc55b06a7744db6
Paulo Cezar Leite dos Santos Junior <103606442+Paulo-PC1@users.noreply.github.com>
2025-02-20 14:56:43 -0300

    Addition without saving the result in memory
    an addition in C++ without saving the result in memory just show then on the screen

---
+++ C02EX_EXTRA01.cpp
@@ -0,0 +1,27 @@
+ //C02EX_EXTRA01.CPP
+
+ #include <iostream>
+
+ using namespace std;
+
+ int main(void)
+ {
+     int A;
+     int B;
+
+     cout << "Entre valor A " << endl;
+     cin >> A;
+     cout << endl;
+
+     cout << "Entre valor B " << endl;
+     cin >> B;
+     cout << endl;
+
+     cout << " Resultado: " << (A) + (B) << endl;
+     cout << endl;
+
+    cout << "Tecle <Enter> para encerrar...";
+     cin.get();
+
+     return 0;
+ }
