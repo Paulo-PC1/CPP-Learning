6a559fa2b940399594b6cb478152ec1deeb2d9c7
Paulo Cezar Leite dos Santos Junior <103606442+Paulo-PC1@users.noreply.github.com>
2025-02-20 14:49:44 -0300

    C++ hello world 2
    test using the line code "using namespace std;"

---
+++ C02EX02.cpp
@@ -0,0 +1,15 @@
+ //C02EX02.CPP
+
+ #include <iostream>
+
+ using namespace std;
+
+ int main(void)
+ {
+     cout << "Ola Mundo" << endl;
+
+     cout << "Tecle <Enter> para encerrar...";
+     cin.get();
+
+     return 0;
+ }
