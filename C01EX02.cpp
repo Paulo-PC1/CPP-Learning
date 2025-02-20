7585154e9d83af275b5cd5f4aa4c4b2fa652ecfd
Paulo Cezar Leite dos Santos Junior <103606442+Paulo-PC1@users.noreply.github.com>
2025-02-20 13:56:24 -0300

    C++ first test
    my firts line codes with  c++

---
+++ C01EX02.cpp
@@ -0,0 +1,12 @@
+ //C01EX02.CPP
+
+ #include <iostream>
+ using namespace std;
+
+ int main(void)
+ {
+     std::cout << "Teste: Code::Blocks" << std::endl;
+     cout << "Telce <Enter> para encerrar...";
+     cin.get();
+     return 0;
+ }
